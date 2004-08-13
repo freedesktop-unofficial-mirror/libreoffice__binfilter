fb1a52462d6d12fba753262f5844fb16b1aeafd1
Oliver Bolte <obo@openoffice.org>
2004-08-13 13:09:13 +0000

    #i10000# MouseEvent to ::com::sun:star::awt:MouseEvent

--- binfilter/bf_forms/source/component/forms_Image.cxx
+++ binfilter/bf_forms/source/component/forms_Image.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: forms_Image.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-03 10:50:48 $
+ *  last change: $Author: obo $ $Date: 2004-08-13 14:09:13 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -338,7 +338,7 @@ Reference< XModel >  OImageControl::getXModel(const InterfaceRef& xIface) const
 //------------------------------------------------------------------------------
 // Diese Methode wird auch aus einem Thread gerufen und muss deshalb
 // thread-safe sein.
-void OImageControl::actionPerformed_Impl(sal_Bool bNotifyListener, const MouseEvent& rEvt)
+void OImageControl::actionPerformed_Impl(sal_Bool bNotifyListener, const ::com::sun::star::awt::MouseEvent& rEvt)
 {
     if( bNotifyListener )
     {
@@ -992,7 +992,7 @@ void OImageControlThread_Impl::processEvent( ::cppu::OComponentHelper *pCompImpl
                                                const Reference<XControl>&,
                                                sal_Bool )
 {
-    ((OImageControl *)pCompImpl)->actionPerformed_Impl( sal_True, *(MouseEvent *)pEvt );
+    ((OImageControl *)pCompImpl)->actionPerformed_Impl( sal_True, *(::com::sun::star::awt::MouseEvent *)pEvt );
 }
 
 //.........................................................................
