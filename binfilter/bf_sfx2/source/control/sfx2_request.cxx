7db7e7cc8fa16e0e71b865571d42baaddb1c6da5
Kurt Zenker <kz@openoffice.org>
2006-11-08 11:26:01 +0000

    INTEGRATION: CWS os71 (1.4.156); FILE MERGED 2006/11/06 16:06:33 os 1.4.156.2: RESYNC: (1.4-1.5); FILE MERGED 2006/03/08 08:41:23 os 1.4.156.1: #i58773# moved legacy filters from sw to binfilter

--- binfilter/bf_sfx2/source/control/sfx2_request.cxx
+++ binfilter/bf_sfx2/source/control/sfx2_request.cxx
@@ -1,17 +1,37 @@
-/*==================================================================
-// class SfxRequest
-//
-// (C) 1996 - 2000 StarDivision GmbH, Hamburg, Germany
-// $Author: rt $ $Date: 2006-10-27 19:10:10 $ $Revision: 1.5 $
-// $Logfile:   T:/bf_sfx2/source/control/request.cxv  $ $Workfile:   REQUEST.CXX  $
-//------------------------------------------------------------------*/
-
-
-
-
-
-
-
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: sfx2_request.cxx,v $
+ *
+ *  $Revision: 1.6 $
+ *
+ *  last change: $Author: kz $ $Date: 2006-11-08 12:26:01 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
 
 #ifndef _SFXITEMITER_HXX //autogen
 #include <svtools/itemiter.hxx>
@@ -25,7 +45,6 @@
 #include <svtools/itemdel.hxx>
 #endif
 
-
 #pragma hdrstop
 
 #include "request.hxx"
@@ -34,9 +53,7 @@
 #include "objface.hxx"
 
 
-#ifndef _SFX_SFXUNO_HXX
-#include "sfxuno.hxx"
-#endif
+#include "appuno.hxx"
 
 namespace binfilter {
 
