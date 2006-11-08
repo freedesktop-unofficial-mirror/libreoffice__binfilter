27689ba7636ffac11041e5eefac7a9495e0f1b58
Kurt Zenker <kz@openoffice.org>
2006-11-08 12:09:41 +0000

    INTEGRATION: CWS os71 (1.6.28); FILE MERGED 2006/11/07 13:00:56 os 1.6.28.3: RESYNC: (1.7-1.8); FILE MERGED 2006/08/30 13:11:33 os 1.6.28.2: RESYNC: (1.6-1.7); FILE MERGED 2005/12/02 10:24:07 os 1.6.28.1: #i58773# move legacy filters to binfilter - first check-in

--- binfilter/inc/bf_sw/doc.hxx
+++ binfilter/inc/bf_sw/doc.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: doc.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 04:38:10 $
+ *  last change: $Author: kz $ $Date: 2006-11-08 13:09:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -71,7 +71,9 @@
 #ifndef _CHCMPRSE_HXX
 #include <chcmprse.hxx>
 #endif
-
+#ifndef _HORIORNT_HXX
+#include <horiornt.hxx>
+#endif
 #ifndef _COM_SUN_STAR_LINGUISTIC2_XSPELLCHECKER1_HPP_
 #include <com/sun/star/linguistic2/XSpellChecker1.hpp>
 #endif
@@ -1094,6 +1096,8 @@ public:
         {   return (SwFrmFmt*)FindFmtByName( (SvPtrarr&)*pSpzFrmFmtTbl, rName ); }
 
     SwCharFmt *MakeCharFmt(const String &rFmtName, SwCharFmt *pDerivedFrom);
+    void       DelCharFmt(sal_uInt16 nFmt);
+    void       DelCharFmt(SwCharFmt* pFmt);
     SwCharFmt* FindCharFmtByName( const String& rName ) const
         {   return (SwCharFmt*)FindFmtByName( (SvPtrarr&)*pCharFmtTbl, rName ); }
 
@@ -1193,6 +1197,7 @@ public:
     sal_Bool IsUsed( const SwNumRule& ) const;
 
         // setze den Namen der neu geladenen Dokument-Vorlage
+    sal_uInt16 SetDocPattern( const String& rPatternName );
         // gebe die Anzahl von geladenen Dok-VorlagenNamen zurueck.
         // !! Die nicht mehr benutzten sind 0-Pointer !!
     sal_uInt16 GetDocPatternCnt() const { return aPatternNms.Count(); }
