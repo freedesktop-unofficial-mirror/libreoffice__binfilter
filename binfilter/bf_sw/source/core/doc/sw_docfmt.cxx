5f34121c429cc10c845a73047a2e751e56050b3e
Kurt Zenker <kz@openoffice.org>
2006-11-08 11:28:29 +0000

    INTEGRATION: CWS os71 (1.7.28); FILE MERGED 2006/11/07 07:07:26 os 1.7.28.2: RESYNC: (1.7-1.8); FILE MERGED 2005/12/02 10:23:46 os 1.7.28.1: #i58773# move legacy filters to binfilter - first check-in

--- binfilter/bf_sw/source/core/doc/sw_docfmt.cxx
+++ binfilter/bf_sw/source/core/doc/sw_docfmt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_docfmt.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:23:36 $
+ *  last change: $Author: kz $ $Date: 2006-11-08 12:28:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -1047,7 +1047,18 @@ struct ParaRstFmt
 /*
  * Loeschen der Formate
  */
+void SwDoc::DelCharFmt(USHORT nFmt)
+{
+    pCharFmtTbl->DeleteAndDestroy(nFmt);
+    SetModified();
+}
 
+void SwDoc::DelCharFmt( SwCharFmt *pFmt )
+{
+    USHORT nFmt = pCharFmtTbl->GetPos( pFmt );
+    ASSERT( USHRT_MAX != nFmt, "Fmt not found," );
+    DelCharFmt( nFmt );
+}
 
 /*N*/ void SwDoc::DelFrmFmt( SwFrmFmt *pFmt )
 /*N*/ {
