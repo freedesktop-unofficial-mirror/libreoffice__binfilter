93337e84d1cc8035d5ae879ecd24c970017217ac
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:35:48 +0000

    INTEGRATION: CWS mba30patches01 (1.4.162); FILE MERGED 2008/04/23 11:53:43 mba 1.4.162.2: RESYNC: (1.4-1.5); FILE MERGED 2008/03/27 15:47:18 mba 1.4.162.1: #i86582#: remove Undo stuff

--- binfilter/inc/bf_sw/ndtxt.hxx
+++ binfilter/inc/bf_sw/ndtxt.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: ndtxt.hxx,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -58,7 +58,6 @@ class SwTxtFmtColl;
 class SwCntntFrm;
 class SwTxtFld;          // Fuer GetTxtFld()
 class SwAttrSet;
-class SwUndoTransliterate;
 
 
 struct SwSpellArgs;             // for Spell(), splargs.hxx
